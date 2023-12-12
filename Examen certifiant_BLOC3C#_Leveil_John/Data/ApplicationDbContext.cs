5e478b538cca9b5fc5669d333d60f9be221206cb
jonda <jonda@DESKTOP-5FE0CE2>
2023-12-12 11:30:03 +0100

    Ajoutez des fichiers projet.

---
+++ Examen certifiant_BLOC3C#_Leveil_John/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Examen_certifiant_BLOC3C__Leveil_John.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
