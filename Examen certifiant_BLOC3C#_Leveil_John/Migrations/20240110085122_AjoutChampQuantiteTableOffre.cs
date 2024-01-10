9abc29423006a034b725bba35a2cfb52ad9c1e23
John Leveil <jondahr@outlook.fr>
2024-01-10 10:27:40 +0100

    Ajout d'une quantité pour les offres

---
+++ Examen certifiant_BLOC3C#_Leveil_John/Migrations/20240110085122_AjoutChampQuantiteTableOffre.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Examen_certifiant_BLOC3C__Leveil_John.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjoutChampQuantiteTableOffre : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantite",
+                table: "Offres",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantite",
+                table: "Offres");
+        }
+    }
+}
