a229f6bf390370b3c16cc7fb7253fb1013fac587
John LEVEIL <john.leveil@ebp.com>
2024-03-01 12:43:35 +0100

    Test page Commandes

---
+++ Test/Areas/Commandes/CommandesTests.cs
@@ -0,0 +1,20 @@
+﻿using Examen_certifiant_BLOC3C__Leveil_John.Areas.Commandes.Pages;
+using Examen_certifiant_BLOC3C__Leveil_John.Data;
+using Examen_certifiant_BLOC3C__Leveil_John.Models;
+using Examen_certifiant_BLOC3C__Leveil_John.Services.QrCodeService;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace Test.Areas.Commandes;
+
+public class CommandesTests
+{
+
+}
